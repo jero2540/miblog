20589d1a270d5f951850ab4861c05d66c3a5380f
jeronimo ibarra hernandez <bromisking25@hotmail.com>
2018-10-09 22:09:27 -0500

    hola

---
+++ universidadautonomadedurango/view/index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Index</title>
+  <link rel="stylesheet" href="../assets/boostrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/menu.css">
+  <script src="../assets/boostrap/js/jquery-3.3.1.min.js"></script>
+  <script src="../assets/boostrap/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="navbar sticky-top flex-md-nowrap p-0">
+    <a class="navbar col-sm-3 col-md-2 mr-0 " href="#"><img src="../assets/img/logo.png" style="max-width: 100%;"></a>
+  </nav>
+
+  <div class="container-fluid">
+    <div class="row">
+      <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+        <div class="sidebar-sticky">
+          <ul class="nav flex-column" style="margin-top: 10px;">
+            <li class="nav-item">
+              <a class="nav-link" href="calculadora.html">
+                <span data-feather="bar-chart-2"></span>
+                Calculadora
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link  "href="libros.html">
+                <span data-feather="layers"></span>
+                Libros
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="servicios">
+                <span data-feather="layers"></span>
+                Posilogia
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link <" href="?c=presupuestos">
+                <span data-feather="file"></span>
+                H.C.
+              </a>
+            </li>
+          </ul>
+        </div>
+        </nav>
+    </div>
+  </div>
+
+
+    <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="../assets/js/jquery-3.1.1.min.js"></script>
+      <script src="../assets/js/bootstrap.min.js"></script>
+      <script src="../assets/js/fontawesome-all.js"></script>
+      <!-- Icons -->
+      <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+      <script>
+        feather.replace()
+      </script>
+
+
+    </body>
+    </html>
